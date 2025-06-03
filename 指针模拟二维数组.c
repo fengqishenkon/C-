ebff2070c0926b800c5d7677030f6f271ffef6d3
风起深空 <15827139224@163.com>
2025-06-03 21:32:23 +0800

    指针实现二维数组

---
+++ 指针模拟二维数组.c
@@ -0,0 +1,24 @@
+//#define _CRT_SECURE_NO_WARNINGS 
+//#include<stdio.h>
+//int main()
+//{
+//	int arr1[] = { 1,2,3,4,5 };
+//	int arr2[] = { 2,3,4,5,6 };
+//	int arr3[] = { 3,4,5,6,7 };
+//
+//	int* arr[] = { arr1,arr2,arr3 };//提问：为什么arr1的类型是int   答：因为arr1里面放的是数字 数字属于整形，所以arr1是int型
+//	                                //提问：为什么arr的类型是 int*  答：因为里面存放的arr1，arr2,arr3都是首元素的地址即整形1，整形2……的地址，所以arr的类型应该为整形指针
+//	int i = 0;
+//	for (i = 0; i < 3; ++i)
+//	{
+//		int b = 0;
+//		for (b = 0; b < 5; ++b)
+//		{
+//			printf("%d ", arr[i][b]);
+//		}
+//		printf("\n");
+//	}
+//
+//		return 0;
+//}
+
